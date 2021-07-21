fba43934ffcf7831f371753cc3e5787db928fad5
r2dev2 <ronak.badhe@gmail.com>
2021-07-21 08:09:20 -0700

    can fork the repo and goto the fork

--- src/main.js
+++ src/main.js
@@ -2,4 +2,23 @@
  * ALSWiki Editor
  */
 
-alert('ALS Wiki Editor loaded');
+const isOnURL = url => () => window.location.href === url;
+const isOnMainWiki = isOnURL('https://github.com/ALSWiki/wiki');
+const isForkedTag = e => e.textContent.includes(`You've already forked wiki`);
+const hasAlreadyForked = () => [...document.querySelectorAll('h3')].some(isForkedTag);
+
+const forkAndGotoFork = () => {
+  document.querySelector('.pagehead-actions li:nth-child(3) summary').click()
+  const interval = setInterval(() => {
+    if (hasAlreadyForked()) {
+      document.querySelector('.button_to button').click();
+      return clearInterval(interval);
+    }
+  }, 50);
+}
+
+window.addEventListener('load', () => {
+  if (isOnMainWiki())
+    return forkAndGotoFork();
+});
+
