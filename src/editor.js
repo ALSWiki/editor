daebc4c9318b6829aa3b3fa633444c73f8e999d2
r2dev2 <ronak.badhe@gmail.com>
2021-07-21 09:52:40 -0700

    create bold button, doesn't do anything

--- src/editor.js
+++ src/editor.js
@@ -3,23 +3,50 @@
 
 #include "./utils.js"
 
-const newFile = URL.includes('/wiki/new/');
+const newFile = URL.includes('/wiki/new/') || URL.includes('/wiki/create/');
 const editingFile = URL.includes('/wiki/edit/');
 const isOnEditor = newFile || editingFile;
+const editor = document.querySelector('.commit-create');
 
 const createArticleNamingHelper = () => {
   const helpInfo = document.createElement('span');
   helpInfo.textContent =
-    ' and name your file as language/Article_Name.md, eg. en/Article_About_ALS.md';
+    'and name your file as language/Article_Name.md, eg. en/Article_About_ALS.md';
   helpInfo.style.marginLeft = '0.25rem';
   document.querySelector('.js-branch-name-label-container').appendChild(helpInfo);
 };
 
+const createBoldButton = () => {
+  const button = document.createElement('button');
+  button.className = 'btn btn-sm';
+  button.textContent = 'B';
+  return button;
+};
+
+const styleMarkdownToolbar = toolbar => {
+  toolbar.style = `
+    display: flex;
+    flex-direction: row wrap;
+    gap: 1rem;
+    padding-left: 2rem;
+  `;
+};
+
+const loadMarkdownHelperToolbar = () => {
+  const toolbar = document.createElement('div');
+  const buttons = [
+    createBoldButton(),
+  ];
+  buttons.forEach(button => toolbar.appendChild(button));
+  styleMarkdownToolbar(toolbar);
+  editor.prepend(toolbar);
+};
+
 const runEditor = () => {
   if (newFile) {
-    console.log('new');
     createArticleNamingHelper();
   }
+  loadMarkdownHelperToolbar();
 };
 
 #endif
